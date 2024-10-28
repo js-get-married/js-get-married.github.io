3edaceaa259e0c3f2b7278ffd0bd516d24ceb54f
randomer108 <jacob.gibbs@gmx.co.uk>
2024-10-28 10:33:52 +0000

    Set to auto rotate

--- coffeemakesmemove.js
+++ coffeemakesmemove.js
@@ -23,19 +23,33 @@ HEADINGS.forEach(heading => {
 // --------------------------------------------------------------------------------------------------------------------------------
 const images = ["assets\\adfc99df-c877-4a71-9c7e-9c7c9e469387.jpg","assets\\IMG_8383.JPEG"];
 let img_index =0;
+let intervalId;
+let timeoutId;
 
 function updateImage() {
     const imgElement =document.getElementById("gallery_image");
     imgElement.src =images[img_index];
 }
 
+function startAutoUpdate() {
+    intervalId = setInterval(nextImg, 2000);
+}
+
+function gallery_pause() {
+    clearInterval(intervalId); 
+    if (timeoutId) clear(timeoutId);
+    timeoutId = setTimeout(startAutoUpdate, 10000); 
+}
+
+
 function prevImg() {
     img_index=(img_index-1+images.length)%images.length
-    updateImage();
+    updateImage();  
 }
 
 function nextImg() {
     img_index=(img_index+1+images.length)%images.length
-    updateImage();
+    updateImage();  
 }
 
+startAutoUpdate();
