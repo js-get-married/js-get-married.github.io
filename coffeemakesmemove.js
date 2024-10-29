bf3f8c362217eddc66f9ff0c07f821daf16dcf40
randomer108 <jacob.gibbs@gmx.co.uk>
2024-10-29 16:47:53 +0000

    picture sliding better

--- coffeemakesmemove.js
+++ coffeemakesmemove.js
@@ -28,31 +28,31 @@ let timeoutId;
 
 function updateImage(direction) {
     const imgElement =document.getElementById("gallery_image");
-    if (direction === "next") {
+    const newImgElement = document.createElement("img");
+
+    if (direction === "init"){
+        imgElement.src = images[0]
+
+    } else if (direction === "next") {
         imgElement.classList.add("slide-left"); // add css property based on direction, which then dictates slide transformation
+        newImgElement.classList.add("slide-left", "new_gallery_image"); 
+
         img_index=(img_index+1+images.length)%images.length
+        newImgElement.src = images[img_index];
+
     } else if (direction === "prev") {
         imgElement.classList.add("slide-right");
         img_index=(img_index-1+images.length)%images.length
     }
+
+    imgElement.parentElement.appendChild(newImgElement);
     // After animation, update the image and reset class
     setTimeout(() => {
+        imgElement.src = images[img_index]; // Now change the main image to the new address
         imgElement.classList.remove("slide-left", "slide-right");
-        imgElement.src = images[img_index];
+        imgElement.classList.remove("slide-left", "slide-right");
+        imgElement.parentElement.removeChild(newImgElement); // Remove the new image element from the DOM
     }, 1000); // Match with CSS transition duration
 
 }
-
-function startAutoUpdate() {
-    intervalId = setInterval(updateImage('next'), 5000);
-}
-
-function gallery_pause() {
-    clearInterval(intervalId); 
-    if (timeoutId) clear(timeoutId);
-    timeoutId = setTimeout(startAutoUpdate, 10000); 
-}
-
-
-
-startAutoUpdate();
+updateImage('init')
